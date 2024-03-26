911a3636fc48ead3bf8d37b5e21ab78ad88977a7
RE <hans.lopezaranda@gmail.com>
2024-03-26 15:56:45 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
-  { 
-  path: '',
-  component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+
 ];
 
 @NgModule({
